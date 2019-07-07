19442146098d9ba1fb044e1b568309b05cd22c63
vadim.nicolaev <vadim.vicolaev.v@gmail.com>
2019-07-07 20:48:33 +0300

    basic commander configs

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('basic configurations');
+/* eslint-disable no-console */
+const commander = require('commander');
+
+commander
+  .version('0.0.1')
+  .option('-n, --new <userName> <userEmail>', 'Add new git user')
+  .option('-c, --current', 'Show current git user');
+
+
+commander
+  .command('help')
+  .action(() => {
+    console.log('Help Docs');
+  });
+
+commander
+  .command('new <userName> <userEmail>')
+  .action((userName, userEmail) => {
+    console.log(userName);
+    console.log(userEmail);
+  });
+
+
+commander.parse(process.argv);
