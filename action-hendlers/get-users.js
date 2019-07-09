f90d507e37c8be07eee1ce2780229c4da4a72ca9
vadim.nicolaev <vadim.vicolaev.v@gmail.com>
2019-07-09 22:49:48 +0300

    format list output

--- action-hendlers/get-users.js
+++ action-hendlers/get-users.js
@@ -1,4 +1,9 @@
 /* eslint-disable no-console */
 const { lowDBService } = require('./../services');
 
-module.exports = () => lowDBService.getUsersList();
+module.exports = () => {
+  const userList = lowDBService.getUsersList();
+  userList.forEach(({ userName, userEmail }) => {
+    console.log(`✔️  username: ${userName} | email: ${userEmail}`);
+  });
+};
