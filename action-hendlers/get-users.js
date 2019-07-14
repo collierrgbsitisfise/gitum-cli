18c841da30e46d292f6f239328c9aa9ad1a2abe3
vadim.nicolaev <vadim.vicolaev.v@gmail.com>
2019-07-14 19:41:52 +0300

    add prefix check only for current users

--- action-hendlers/get-users.js
+++ action-hendlers/get-users.js
@@ -1,9 +1,18 @@
 /* eslint-disable no-console */
 const { lowDBService } = require('./../services');
+const { getCurrentGitUser } = require('./../utils');
 
-module.exports = () => {
+module.exports = async () => {
   const userList = lowDBService.getUsersList();
+  const currentGitUser = await getCurrentGitUser();
+
   userList.forEach(({ userName, userEmail }) => {
-    console.log(`✔️  username: ${userName} | email: ${userEmail}`);
+    let prefix = '';
+
+    if (currentGitUser.userName === userName && currentGitUser.userEmail === userEmail) {
+      prefix = '✔️';
+    }
+
+    console.log(`${prefix}  username: ${userName} | email: ${userEmail}`);
   });
 };
